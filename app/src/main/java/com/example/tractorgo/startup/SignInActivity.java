3d440a01d2d0d05bba5db0def4e9e970c712dcf0
Sibi Chakravarthy P <66554485+SibiChakravarthy7311@users.noreply.github.com>
2021-03-16 00:51:53 +0530

    Toolbar Binding First Update

--- app/src/main/java/com/example/tractorgo/startup/SignInActivity.java
+++ app/src/main/java/com/example/tractorgo/startup/SignInActivity.java
@@ -1,16 +1,48 @@
 package com.example.tractorgo.startup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.tractorgo.R;
+import com.example.tractorgo.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        //binding= DataBindingUtil.setContentView(this, R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        setSupportActionBar(binding.toolbarSignIn);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_signup_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch(id)
+        {
+            case R.id.loginItem:
+                Toast.makeText(this, "Sign in", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
