a77b15f2068d24e290b2e4ee86d115d8675fd39a
Sibi Chakravarthy P <66554485+SibiChakravarthy7311@users.noreply.github.com>
2021-03-16 11:43:11 +0530

    Toolbar White TextColor

--- app/src/main/java/com/example/tractorgo/startup/SignInActivity.java
+++ app/src/main/java/com/example/tractorgo/startup/SignInActivity.java
@@ -4,8 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,7 +24,6 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //binding= DataBindingUtil.setContentView(this, R.layout.activity_sign_in);
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
@@ -32,6 +34,15 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_signup_activity, menu);
+        for(int i=0; i<menu.size(); i++){
+            MenuItem menuItem = menu.getItem(i);
+            SpannableString spannable = new SpannableString(
+                    menu.getItem(i).getTitle().toString()
+            );
+            spannable.setSpan(new ForegroundColorSpan(Color.WHITE),
+                    0, spannable.length(), 0);
+            menuItem.setTitle(spannable);
+        }
         return true;
     }
 
